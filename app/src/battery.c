6f0aaf80d099665db33b7f7ba16ea768aefefcda
Alex Dunphy <alexanderdunphy@gmail.com>
2022-01-10 18:53:49 +0000

    Test disabling battery reporting

--- app/src/battery.c
+++ app/src/battery.c
@@ -89,7 +89,7 @@ static int zmk_battery_init(const struct device *_arg) {
         return rc;
     }
 
-    k_timer_start(&battery_timer, K_MINUTES(1), K_MINUTES(1));
+    // k_timer_start(&battery_timer, K_MINUTES(1), K_MINUTES(1));
 
     return 0;
 }
